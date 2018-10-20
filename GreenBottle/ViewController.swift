07de59c6781771bbfbbfba7c37b47d08bdbb6fae
Alexis Celestino Solís <alex@Binarys-MacBook-Pro.local>
2018-10-20 09:48:57 -0500

    first commit

---
+++ GreenBottle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreenBottle
+//
+//  Created by Alexis Celestino Solís on 10/20/18.
+//  Copyright © 2018 Basic Kraken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
