488393a74e364d1dfaba363a904cb56936f0507c
fer96 <fer96.dlrs@gmail.com>
2018-10-20 22:56:35 -0500

    Add simple detail view of a beer

--- GreenBottle/Beer.swift
+++ GreenBottle/Beer.swift
@@ -16,8 +16,8 @@ struct Beer{
     
     static func loadSampleBeers() ->[Beer]{
         let beer = Beer(name: "Indio", slogan: "Orgullosamente Indio", porcent: "90", info: "Lorem.....")
-        let beer2 = Beer(name: "Carta Blanca", slogan: "La mejor desde los 50's", porcent: "75", info: "Lorem....")
-        let beer3 = Beer(name: "Modelo", slogan: "La cerveza obscura de México", porcent: "40", info: "Lorem.....")
+        let beer2 = Beer(name: "Carta Blanca", slogan: "La mejor desde los 50's", porcent: "55", info: "Lorem....")
+        let beer3 = Beer(name: "Modelo", slogan: "La cerveza obscura de México", porcent: "20", info: "Lorem.....")
         
         return [beer, beer2, beer3]
     }
